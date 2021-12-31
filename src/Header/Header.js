17194a44beb22229373125027479573315e8d719
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2022-01-01 01:23:35 +0530

    Update title

---
+++ src/Header/Header.js
@@ -0,0 +1,9 @@
+import classes from './Header.module.css'
+
+function Header() {
+    return <div className={classes.header}>
+        <span>Quiz Time</span>
+    </div>
+}
+
+export default Header;
